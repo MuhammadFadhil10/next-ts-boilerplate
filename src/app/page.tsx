6405e8ddec182b0051e2ca02c75f99f0128b4a9f
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-08-03 23:40:04 +0700

    chore: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { A } from "./A";
-import { B } from "./B";
-
 export default function Home() {
-
   return (
     <main>
       <div>
-        <A />
-        <B />
         <h1>Your boilerplate!</h1>
       </div>
     </main>
