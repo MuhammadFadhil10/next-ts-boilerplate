5985b6101e4c76cf40e79b73d6a34308ab5c8972
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-08-14 10:46:08 +0700

    feat: add tanstack client provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
-import './globals.css'
+import "./globals.css";
+import TanstackProvider from "@/providers/TanstackProvider";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <TanstackProvider>
+        <body>{children}</body>
+      </TanstackProvider>
     </html>
-  )
+  );
 }
